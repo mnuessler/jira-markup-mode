726ba9e2ba9a809856bbb90d68177cc61c086d5d
Matthias Nuessler <m.nuessler@web.de>
2012-09-27 23:46:38 +0200

    Try to install emacs-snapshot from 3rd-party-PPA before travis build.

--- run-travis-ci.sh
+++ run-travis-ci.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
 if [ `uname` = "Darwin" ]; then
+  # for local testing only
   EMACS="/Applications/Emacs.app/Contents/MacOS/Emacs"
+elsif [ `which emacs-snapshot` != "" ]; then
+  EMACS="$(which emscs-snapshot)"
 else
   EMACS="$(which emacs)"
 fi
 
-echo "Using emacs: $EMACS"
+echo "Found emacs: $EMACS"
 $EMACS --version
 
-$EMACS -batch -l ert -l jira-markup-mode.el -l jira-markup-mode-test.el -f ert-run-tests-batch-and-exit
+$EMACS -batch \
+-l ert \
+-l jira-markup-mode.el \
+-l jira-markup-mode-test.el \
+-f ert-run-tests-batch-and-exit
 
